c4ecc73a30923579fe0c58533dbba56debedcc78
natikagrin <99361467+natikagrin@users.noreply.github.com>
2022-08-02 20:08:34 +0300

    на перевірку без 10 завдання

--- js/task-09.js
+++ js/task-09.js
@@ -1,6 +1,6 @@
 //Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
 function getRandomHexColor() {
-  return `${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 const changeCollorButton = document.querySelector('.change-color');
 const colorValue = document.querySelector('.color');
