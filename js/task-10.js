04280d31350e535889a5616e2fe0293535da8fcc
natikagrin <99361467+natikagrin@users.noreply.github.com>
2022-08-08 08:38:51 +0300

    **

--- js/task-10.js
+++ js/task-10.js
@@ -5,28 +5,80 @@
     //Розміри найпершого <div> - 30px на 30px.
    // Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
     //Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору
-    function getRandomHexColor() {
+   /* function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)}`;
+}*/
+
+const refs = {
+  input: document.querySelector("input"),
+  createBtn: document.querySelector("button[data-create]"),
+  destroyBtn: document.querySelector("button[data-destroy]"),
+
+  numberOfElements: [],
+  objectOfElement: {},
+  arrayNumberOfDiv: [],
+
+  div: document.querySelector("div#boxes"),
+};
+
+refs.input.addEventListener("input", onHandlerInput);
+refs.createBtn.addEventListener("click", onHandlerCreateBtn);
+refs.destroyBtn.addEventListener("click", onHandlerDestroyBtn);
+
+function onHandlerInput(event) {
+  event.preventDefault();
+
+  refs.objectOfElement[event.target.type] = event.target.value;
+  refs.arrayNumberOfDiv.push(refs.objectOfElement.number);
 }
-const root = document.getElementById('root');
-function createBoxes(amount) {
-  const basicSize = 30;
-  const fragment = document.createDocumentFragment();
-  for (let i = 0; i < amount; i++) {
-    const size = basicSize + i * 10;
-    const div = document.createElement('div');
-    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
-    fragment.appendChild(div);
+
+function onHandlerCreateBtn(event) {
+  event.preventDefault();
+
+  if (refs.input.value === "") {
+    return alert("Все поля должны быть заполнены");
   }
-  root.appendChild(fragment);
+
+  refs.numberOfElements.push(refs.objectOfElement);
+  refs.objectOfElement = {};
+  refs.input.value = "";
+
+  createBoxes(refs.arrayNumberOfDiv);
+  refs.arrayNumberOfDiv.length = 0;
+
+  console.log("Кнопка СОЗДАТЬ: ", refs.numberOfElements);
+}
+
+function onHandlerDestroyBtn(event) {
+  event.preventDefault();
+
+  refs.numberOfElements.length = 0;
+  refs.arrayNumberOfDiv.length = 0;
+  refs.input.value = "";
+
+  destroyBoxes();
+  console.log("Кнопка ОЧИСТИТЬ: ", refs.numberOfElements);
+}
+
+function createBoxes(amount) {
+  amount.forEach((el) => {
+    const div = document.createElement("div");
+    div.style.padding = "30px";
+
+    div.style.backgroundColor = `${getRandomHexColor()}`;
+
+    refs.div.append(div);
+
+    div.textContent = amount;
+    div.textContent = "";
+  });
 }
 
 function destroyBoxes() {
-  root.textContent = '';
+  refs.div.innerHTML = "";
 }
 
-function random() {
-  return Math.floor(Math.random() * 256);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-createBoxes(6)
